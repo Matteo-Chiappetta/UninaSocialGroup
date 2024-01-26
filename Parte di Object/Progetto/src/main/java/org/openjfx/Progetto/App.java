5e7c454b85d774808a773795356e7910d1dd1ee1
Matteo-sudo <MattyC99@outlook.it>
2024-01-26 18:51:35 +0100

    Si inizia con il progetto

--- Parte di Object/Progetto/src/main/java/org/openjfx/Progetto/App.java
+++ Parte di Object/Progetto/src/main/java/org/openjfx/Progetto/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
         stage.show();
     }
 
-    public static void setRoot(String fxml) throws IOException {
+    static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
