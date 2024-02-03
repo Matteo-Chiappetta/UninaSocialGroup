e45c07d33896a580a44c442f39504e929988cf2b
Matteo-sudo <MattyC99@outlook.it>
2024-02-03 18:28:01 +0100

    Rimesso il progetto

---
+++ Parte di Object/Progetto/src/main/java/org/openjfx/Progetto/App.java
@@ -0,0 +1,51 @@
+package org.openjfx.Progetto;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+<<<<<<< HEAD
+        scene = new Scene(loadFXML("ProvaFinestraLogin"), 873, 609);
+=======
+        scene = new Scene(loadFXML("ProvaFinestraLogin"), 640, 480);
+>>>>>>> 50e2d1aaff0b0f629460a279fc42e7cbad9ac9bf
+        //String css = this.getClass().getResource("Tema.css").toExternalForm();
+        //scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.setTitle("Unina Social Group");
+        //stage.getIcons().add(new Image("file:icon.png"));
+        Image logo = new Image("USG.jpg");
+        stage.getIcons().add(logo);
+        stage.setTitle("UninaSocialGroup - Sing In");
+        stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+    	//System.out.println(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ Parte di Object/Progetto/src/main/java/org/openjfx/Progetto/Entity/MiPiace.java
@@ -0,0 +1,5 @@
+package org.openjfx.Progetto.Entity;
+
+public class MiPiace extends Interazione {
+	// Vedere come rappresentare in java la relazione post e mi piace
+}
